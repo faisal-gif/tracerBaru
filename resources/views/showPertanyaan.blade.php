6ff8be75fe230c96369baa9e1ee8726f73df45b9
Faisal <fagdera@gmai.com>
2022-06-10 15:58:22 +0700

    new31

--- resources/views/showPertanyaan.blade.php
+++ resources/views/showPertanyaan.blade.php
@@ -24,7 +24,7 @@
        
                         <tr>
                         <td>
-                        {{ Str::limit(p->label, 50),$end='......' }}
+                        {{ Str::limit( $p->label, 50),$end='......' }}
                           </td>
                           <td>
                           {{$p->type}}
